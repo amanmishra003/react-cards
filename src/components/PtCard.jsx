c175cd25974c0c963e1ceef5b36f3dc79b0f3faa
Aman Mishra <amanmishra4191@gmail.com>
2023-03-07 17:49:40 -0800

    Update PtCard.jsx

--- src/components/PtCard.jsx
+++ src/components/PtCard.jsx
@@ -5,7 +5,7 @@ function PtCard(props) {
     <div className="property-card">
       <img
         className="property-img"
-        src="https://images.unsplash.com/photo-1600585154340-be6161a56a0c?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80"
+        src={props.image}
         alt="Property"
       />
 
